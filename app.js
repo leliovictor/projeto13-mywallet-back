4de2155ff49c7a606f5490214a3e4babaad7edec
Lelio Victor <leliov.vieira@gmail.com>
2023-05-31 14:19:55 -0300

    chore: :hammer: added error handler

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import router from "./routes/index.js";
-//import errorHandler from "./events/error.js";
+import errorHandler from "./events/error.js";
 
 dotenv.config();
 
@@ -12,7 +12,7 @@ const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
-//app.use(errorHandler); adicionar um erro handler
+app.use(errorHandler);
 
 export default app;
 
